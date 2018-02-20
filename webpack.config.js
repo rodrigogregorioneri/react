0cf6285170c132dfe458f31919617ed95a81496e
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2018-02-20 08:37:10 -0300

    configuração inicial webpack para react

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/index.js',
-    output:{
-        path:  __dirname + '/dist',
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + './public',
         filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+            }
+        }]
     }
 }
